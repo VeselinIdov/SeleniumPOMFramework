38cbc926ab70b94161e5995718696bd96acf7e59
VeselinIdov <58400694+VeselinIdov@users.noreply.github.com>
2021-03-21 17:21:10 +0200

    updated chrome driver

--- src/test/java/configurations/BasePage.java
+++ src/test/java/configurations/BasePage.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
 	@FindBy(css = "dsdas")
 	private WebElement globalTest;

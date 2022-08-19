3f6b39a2137acae43e9007defd87e018dec4415c
VeselinIdov <58400694+VeselinIdov@users.noreply.github.com>
2022-08-20 00:52:41 +0300

    added second class

---
+++ src/test/java/tests/runningTests2.java
@@ -0,0 +1,28 @@
+package tests;
+
+import configurations.BaseTest;
+import org.testng.annotations.Test;
+
+public class runningTests2 extends BaseTest {
+
+    @Test
+    void testDropdown()throws Exception  {
+        System.out.println("Test 1");
+
+    }
+
+    @Test
+    void testDropdown1()throws Exception  {
+        System.out.println("Test 2");
+    }
+
+    @Test
+    void testDropdown2()throws Exception  {
+        System.out.println("Test 3");
+    }
+
+    @Test
+    void testDropdown3()throws Exception  {
+        System.out.println("Test 4");
+    }
+}
